ce5b6b58bcf81751c5dedf495712099966006983
jinaxlee <mimi99472@gmail.com>
2024-03-15 12:48:57 +0900

    finish footer

--- js/custom.js
+++ js/custom.js
@@ -51,10 +51,34 @@ document.addEventListener("DOMContentLoaded", function () {
       start: "top bottom",
       scrub: 1.8,
     },
+    benefits: {
+      trigger: ".benefits_lists",
+      start: "top bottom",
+      scrub: 1.8,
+    },
+
+    work: {
+      trigger: ".work",
+      start: "top bottom",
+      scrub: 1.8,
+    },
+
+    service: {
+      trigger: ".service",
+      start: "top bottom",
+      scrub: 1.8,
+    },
+
+    footer: {
+      trigger: "footer",
+      start: "top bottom",
+      end: "bottom bottom",
+      scrub: 1.8,
+    },
   };
 
   // square rotate animation
-  // const titleSquare = document.querySelector(".title-square");
+  // const titleSquare = document.querySelector('.title-square');
   const titleSquares = gsap.utils.toArray(".title-square");
 
   titleSquares.forEach((square) => {
@@ -130,17 +154,71 @@ document.addEventListener("DOMContentLoaded", function () {
 
   aboutAnimation();
 
-  function benefitsAnimation(){
+  function benefitsAnimation() {
     const benefits_nums = gsap.utils.toArray(".benefits_num");
-    console.log(benefits_nums);
 
     benefits_nums.forEach((num) => {
-      const data_speed = num.getAttribute('data-speed');
-      console.log(data_speed);
-    })
+      const data_speed = num.getAttribute("data-speed");
+
+      tl.from(num, {
+        scrollTrigger: commonScrollTrigger.benefits,
+        x: -data_speed,
+      });
+    });
   }
 
   benefitsAnimation();
+
+  function workAnimation() {
+    const work_elmts = gsap.utils.toArray(".work-item, .work-item-num");
+    console.log(work_elmts);
+
+    work_elmts.forEach((num) => {
+      const data_speed = num.getAttribute("data-speed");
+
+      tl.from(num, {
+        scrollTrigger: commonScrollTrigger.work,
+        y: -data_speed,
+      });
+    });
+
+    tl.from(".work-item-image img", {
+      scrollTrigger: commonScrollTrigger.work,
+      scale: 1.6,
+    });
+  }
+
+  workAnimation();
+
+  function serviceAnimation() {
+    const arrow_elmts = gsap.utils.toArray(".service-arrow");
+
+    arrow_elmts.forEach((num) => {
+      const data_speed = num.getAttribute("data-speed");
+
+      tl.from(num, {
+        scrollTrigger: commonScrollTrigger.service,
+        x: -data_speed,
+      });
+    });
+  }
+
+  serviceAnimation();
+
+  function footerAnimation() {
+    const letter_elmts = gsap.utils.toArray(".footer-wrapper span");
+
+    letter_elmts.forEach((num) => {
+      const data_speed = num.getAttribute("data-speed");
+
+      tl.from(num, {
+        scrollTrigger: commonScrollTrigger.footer,
+        y: -data_speed,
+      });
+    });
+  }
+
+  footerAnimation();
 
   const wWidth = window.outerWidth;
 
@@ -154,10 +232,14 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 const arr = ["홍콩반점", "오복성", "동보성"]; // 배열
+
 console.log(arr[0]);
 
 const obj = {
-  홍콩반점: { 짜장면: 5000, 짬뽕: 6000 },
+  홍콩반점: {
+    짜장면: 5000,
+    짬뽕: 6000,
+  },
   오복성: ["짜장면", "짬뽕"],
   동보성: ["짜장면", "짬뽕"],
 };
